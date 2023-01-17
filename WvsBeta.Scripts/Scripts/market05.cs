346c4fc05fa1a04b23c9091d23cc8321024911da
Simon Haga <1498414+sewil@users.noreply.github.com>
2023-01-17 09:04:50 +0100

    Market portal scripts

---
+++ WvsBeta.Scripts/Scripts/market05.cs
@@ -0,0 +1,15 @@
+﻿using WvsBeta.Game;
+using WvsBeta.Game.Scripting;
+
+namespace WvsBeta.Scripts.Scripts
+{
+    internal class Market05 : IPortalScript
+    {
+        public void Run(IPortalHost host, GameCharacter target)
+        {
+            var quest = ScriptStandard.GetQuestData(target, ScriptStandard.ScriptQuest.Portal);
+            quest.Data = "3";
+            target.ChangeMap(910000000, "st00");
+        }
+    }
+}
---
+++ WvsBeta.Scripts/Scripts/market08.cs
@@ -0,0 +1,15 @@
+﻿using WvsBeta.Game;
+using WvsBeta.Game.Scripting;
+
+namespace WvsBeta.Scripts.Scripts
+{
+    internal class Market08 : IPortalScript
+    {
+        public void Run(IPortalHost host, GameCharacter target)
+        {
+            var quest = ScriptStandard.GetQuestData(target, ScriptStandard.ScriptQuest.Portal);
+            quest.Data = "7";
+            target.ChangeMap(910000000, "st00");
+        }
+    }
+}
