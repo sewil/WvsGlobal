0d7084e74d41b2a834f3f6131126d36e8635f492
Simon Haga <1498414+sewil@users.noreply.github.com>
2023-03-12 02:17:23 +0100

    Create BitExtensions.cs

---
+++ WvsBeta.Common/Extensions/BitExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WvsBeta.Common.Extensions
+{
+    public static class BitExtensions
+    {
+        public static uint RotateLeft(this uint value, int count)
+        {
+            return (value << count) | (value >> (32 - count));
+    }
+
+        public static uint RotateRight(this uint value, int count)
+        {
+            return (value >> count) | (value << (32 - count));
+        }
+    }
+}
