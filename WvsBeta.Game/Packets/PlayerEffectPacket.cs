ed4f9c9f36c25c6063b35b23c93117d2fb811067
Simon Haga <1498414+sewil@users.noreply.github.com>
2023-03-29 19:16:50 +0200

    Inventory changed effect

--- WvsBeta.Game/Packets/PlayerEffectPacket.cs
+++ WvsBeta.Game/Packets/PlayerEffectPacket.cs
@@ -6,8 +6,11 @@ namespace WvsBeta.Game.Packets
     {
         SkillOnSelf = 1,
         SkillOnOther = 2,
+        QuestEffect = 3,
+        InventoryChanged = 3,
         Pet = 4,
         UseEXPCharm = 6,
+        JobChanged = 8,
     }
     public enum PetEffectType
     {
@@ -79,6 +82,33 @@ namespace WvsBeta.Game.Packets
             if (!isSafetyCharm)
             {
                 pw.WriteInt(itemid);
+            }
+            pw.Send(false, true);
+        }
+        /// <summary>
+        /// Beware: References Effect/Quest.img/%d which doesn't exist
+        /// </summary>
+        public static void SendQuestEffect(GameCharacter chr, string effect, int effectid)
+        {
+            var pw = new PlayerEffectPacket(chr, PlayerEffectType.QuestEffect);
+            pw.WriteByte(0);
+            pw.WriteString(effect);
+            pw.WriteInt(effectid);
+            pw.Send(false, true);
+        }
+        /// <summary>
+        /// Sends grey text items gained/lost. Max 255 items.
+        /// </summary>
+        public static void SendInventoryChanged(GameCharacter chr, params (int itemid, short amount)[] items)
+        {
+            if (items.Length == 0) return; // Prevent triggering quest effect
+
+            var pw = new PlayerEffectPacket(chr, PlayerEffectType.InventoryChanged);
+            pw.WriteByte((byte)items.Length);
+            foreach (var (itemid, amount) in items)
+            {
+                pw.WriteInt(itemid);
+                pw.WriteInt(amount);
             }
             pw.Send(false, true);
         }
