372eeeae482a00a842e9260a467dc2629d217eed
sewil <1498414+sewil@users.noreply.github.com>
2023-04-22 10:30:05 +0200

    Create UserCommonPacket.cs

---
+++ WvsBeta.Common/Packets/UserCommonPacket.cs
@@ -0,0 +1,12 @@
+﻿using WvsBeta.Common.Sessions;
+
+namespace WvsBeta.Common.Packets
+{
+    public class UserCommonPacket : Packet
+    {
+        public UserCommonPacket(ServerMessages msg, int charId) : base(msg)
+        {
+            WriteInt(charId);
+        }
+    }
+}
