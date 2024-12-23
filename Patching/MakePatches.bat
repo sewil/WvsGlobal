933bbb15d6ef75dc3b808bb86db6bbf83f0ea128
sewil <1498414+sewil@users.noreply.github.com>
2024-12-23 13:05:08 +0100

    Update MakePatches.bat

--- Patching/MakePatches.bat
+++ Patching/MakePatches.bat
@@ -3,4 +3,4 @@ rmdir /s /q patches\
 mkdir patches\
 copy patch.base patches\
 copy ..\ClientBin\Patcher.exe patches\NewPatcher.dat
-rem ..\WvsBeta.PatchCreator\bin\Release\WvsBeta.PatchCreator.exe make-patches
+..\WvsBeta.PatchCreator\bin\Release\WvsBeta.PatchCreator.exe make-patches
