0caa43c1ad12c6f72833c8bd5059402e385183c1
sewil <1498414+sewil@users.noreply.github.com>
2024-12-24 11:31:00 +0100

    Fix Olaf text typo (#138)

--- ClientBin/DataWZ_GeneratePatch.bat
+++ ClientBin/DataWZ_GeneratePatch.bat
@@ -1,3 +1,7 @@
 @echo off
 rem Usage: DataWZ_GeneratePatch.bat old-in new-in patch-out
-..\Patching\courgette64.exe -gen %1 %2 %3
+set /p oldIn=Path in to old Data.wz: 
+set /p newIn=Path in to new Data.wz: 
+set /p patchOut=Path out to .patch file: 
+..\Patching\courgette64.exe -gen %oldIn% %newIn% %patchOut%
+pause
