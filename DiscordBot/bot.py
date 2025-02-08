708abf234003259c8a9a20372e5156b7319ffb10
sewil <1498414+sewil@users.noreply.github.com>
2025-02-08 14:01:37 +0100

    Discord Bot RSVP registration

--- DiscordBot/bot.py
+++ DiscordBot/bot.py
@@ -1,3 +1,5 @@
+import mariadb
+from typing import Literal
 import discord
 import os
 from discord import app_commands
@@ -27,12 +29,22 @@ DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
 
+RSVP_DB_HOST = os.getenv("RSVP_DB_HOST")
+RSVP_DB_NAME = os.getenv("RSVP_DB_NAME")
+RSVP_DB_USER = os.getenv("RSVP_DB_USER")
+RSVP_DB_PASS = os.getenv("RSVP_DB_PASS")
+
 class RegisterModal(discord.ui.Modal, title="Register"):
-    # Define form fields
+    server: str
     username = discord.ui.TextInput(label="Username", placeholder="Manji")
     dob = discord.ui.TextInput(label="Date of birth (For char deletion)", placeholder="YYYY-MM-DD")
     password = discord.ui.TextInput(label="Password", placeholder="*****", min_length=4, max_length=12)
     password2 = discord.ui.TextInput(label="Password (again)", placeholder="*****", min_length=4, max_length=12)
+
+    def __init__(self, server: str):
+        super().__init__()
+        self.server = server
+        self.title = f"Register ({server})"
 
     async def on_submit(self, interaction: discord.Interaction):
         message = f'Something went wrong!'
@@ -41,10 +53,7 @@ class RegisterModal(discord.ui.Modal, title="Register"):
         elif re.match(r"^(19|20)\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$", self.dob.value) == None:
             message = "Invalid date of birth!"
         else:
-            # Get GM Level
-            member: discord.Member = interaction.user
-            gm_level = get_gm_level(member.roles)
-            message = db_register(interaction, self, gm_level)
+            message = db_register(interaction, self)
 
         # Handle the form submission
         await interaction.response.send_message(
@@ -54,21 +63,25 @@ class RegisterModal(discord.ui.Modal, title="Register"):
 
 
 class ChangePasswordModal(discord.ui.Modal, title="Change password"):
-    # Define form fields
+    server: str
     username = discord.ui.TextInput(label="Username", placeholder="Manji")
     current_password = discord.ui.TextInput(label="Current password", placeholder="*****")
     new_password = discord.ui.TextInput(label="New password", placeholder="*****", min_length=4, max_length=12)
     new_password2 = discord.ui.TextInput(label="New password (again)", placeholder="*****", min_length=4, max_length=12)
 
+    def __init__(self, server: str):
+        super().__init__()
+        self.server = server
+        self.title = f"Change password ({server})"
+
     async def on_submit(self, interaction: discord.Interaction):
         message = f'Something went wrong!'
-        user_id = interaction.user.id
         if self.new_password.value != self.new_password2.value:
             message = "Mismatching passwords!"
         elif self.current_password.value == self.new_password.value:
             message = "Can't change to the same password!"
         else:
-            message = db_change_password(user_id, self)
+            message = db_change_password(interaction, self)
 
         # Handle the form submission
         await interaction.response.send_message(
@@ -76,36 +89,62 @@ class ChangePasswordModal(discord.ui.Modal, title="Change password"):
             ephemeral=True
         )
 
-def db_connect():
-    cnx = mysql.connector.connect(
-        host=DB_HOST,
-        port=3306,
-        user=DB_USER,
-        password=DB_PASS,
-        database=DB_NAME
-    )
-    cur = cnx.cursor()
-    return (cnx, cur)
+def db_connect(form):
+    if form.server == "rsvp":
+        cnx = mariadb.connect(
+            user=RSVP_DB_USER,
+            password=RSVP_DB_PASS,
+            host=RSVP_DB_HOST,
+            database=RSVP_DB_NAME,
+            port=3306
+        )
+        cur = cnx.cursor()
+        return (cnx, cur)
+    else:
+        host = DB_HOST
+        user = DB_USER
+        pw = DB_PASS
+        name = DB_NAME
+        cnx = mysql.connector.connect(
+            host=host,
+            port=3306,
+            user=user,
+            password=pw,
+            database=name,
+        )
 
-def db_register(interaction, form, gm_level):
+        cur = cnx.cursor()
+
+        return (cnx, cur)
+
+def db_register(interaction: discord.Interaction, form: RegisterModal):
     try:
-        (cnx, cur) = db_connect()
+        user_id = interaction.user.id
+        is_rsvp = form.server == "rsvp"
+        dob_formatted = int(form.dob.value.replace("-", ""))
+        hashed_password = bcrypt.hashpw(form.password.value.encode(), bcrypt.gensalt(13 if is_rsvp else 10, prefix=b'2a'))
+
+        if is_rsvp:
+            insert_query = "INSERT INTO users (username, password, email, gender, admin, char_delete_password) VALUES (%s, %s, %s, %s, %s, %s)"
+            query_params = (form.username.value, hashed_password, user_id, 10, 0, dob_formatted)
+        else:
+            member: discord.Member = interaction.user
+            gm_level = get_gm_level(member.roles)
+            insert_query = "INSERT INTO users (username, discord_id, password, email, gender, gm, char_delete_password) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+            query_params = (form.username.value, user_id, hashed_password, '', 10, gm_level, dob_formatted)
+
+        (cnx, cur) = db_connect(form)
 
         # Check username/discord_id already exists
-        user_id = interaction.user.id
-        check_query = """
+        check_query = f"""
             SELECT COUNT(*) FROM users
-            WHERE LOWER(username) = LOWER(%s) OR LOWER(discord_id) = LOWER(%s)
+            WHERE LOWER(username) = LOWER(%s) OR LOWER({"email" if is_rsvp else "discord_id"}) = LOWER(%s)
         """
         cur.execute(check_query, (form.username.value, user_id))
         if cur.fetchone()[0] > 0:
             return "This user is already registered!"
 
-        dob_formatted = int(form.dob.value.replace("-", ""))
-        hashed_password = bcrypt.hashpw(form.password.value.encode(), bcrypt.gensalt(10, prefix=b'2a'))
-
-        insert_query = "INSERT INTO users (username, discord_id, password, email, gender, gm, char_delete_password) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-        cur.execute(insert_query, (form.username.value, user_id, hashed_password, '', 10, gm_level, dob_formatted))
+        cur.execute(insert_query, query_params)
         cnx.commit()
 
         return f'Welcome {form.username}!'
@@ -116,26 +155,30 @@ def db_register(interaction, form, gm_level):
         if 'cur' in locals(): cur.close()
         if 'cnx' in locals(): cnx.close()
 
-def db_change_password(user_id, form):
+def db_change_password(interaction: discord.Interaction, form: ChangePasswordModal):
     try:
-        (cnx, cur) = db_connect()
+        user_id = interaction.user.id
+
+        (cnx, cur) = db_connect(form)
+
+        is_rsvp = form.server == "rsvp"
 
         # Check username/discord_id combination exists
-        check_query = """
+        check_query = f"""
             SELECT * FROM users
-            WHERE LOWER(username) = LOWER(%s) AND LOWER(discord_id) = LOWER(%s)
+            WHERE LOWER(username) = LOWER(%s) AND LOWER({"email" if is_rsvp else "discord_id"}) = LOWER(%s)
         """
         cur.execute(check_query, (form.username.value, user_id))
         user = cur.fetchone()
         if user == None:
             return "User not found! Make sure to use the same Discord account that you registered with."
 
-        db_pass = user[3].encode()
+        db_pass = user[2 if is_rsvp else 3].encode()
         hashed_password = bcrypt.hashpw(form.current_password.value.encode(), db_pass)
         if hashed_password != db_pass:
             return 'Wrong password!'
 
-        hashed_new_password = bcrypt.hashpw(form.new_password.value.encode(), bcrypt.gensalt(10, prefix=b'2a'))
+        hashed_new_password = bcrypt.hashpw(form.new_password.value.encode(), bcrypt.gensalt(13 if is_rsvp else 10, prefix=b'2a'))
 
         insert_query = "UPDATE users SET password=%s WHERE ID=%s"
         cur.execute(insert_query, (hashed_new_password, user[0]))
@@ -156,9 +199,9 @@ tree = app_commands.CommandTree(client)
 guild = discord.Object(id=SERVER_ID)
 
 @tree.command(name="register", description="Opens a registration modal.", guild=guild)
-async def register(interaction: discord.Interaction):
+async def register(interaction: discord.Interaction, server: Literal["rsvp", "openmg"]):
     # Instantiate the modal
-    modal = RegisterModal()
+    modal = RegisterModal(server)
     # Get GM Level
     member: discord.Member = interaction.user
     gm_level = get_gm_level(member.roles)
@@ -172,8 +215,8 @@ async def register(interaction: discord.Interaction):
         await interaction.response.send_modal(modal)
 
 @tree.command(name="change_password", description="Opens a change password modal.", guild=guild)
-async def changePassword(interaction: discord.Interaction):
-    modal = ChangePasswordModal()
+async def changePassword(interaction: discord.Interaction, server: Literal["rsvp", "openmg"]):
+    modal = ChangePasswordModal(server)
     await interaction.response.send_modal(modal)
 
 @client.event
